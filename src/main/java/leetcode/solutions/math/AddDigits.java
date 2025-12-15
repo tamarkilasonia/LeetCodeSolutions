7f87f761e654036924c671c63df5299d8886e20e
tamarkilasonia <t.kilasonia2006@gmail.com>
2025-12-15 12:00:00 +0400

    Add Digits

---
+++ src/main/java/leetcode/solutions/math/AddDigits.java
@@ -0,0 +1,10 @@
+package leetcode.solutions.math;
+
+public class AddDigits {
+
+        public int addDigits(int num) {
+            if (num == 0) return 0;
+            return (num - 1) % 9 + 1;
+        }
+    }
+
