a714d1d0cc47acbe7a067ae974444924ab4d425f
tamarkilasonia <t.kilasonia2006@gmail.com>
2026-02-14 12:00:00 +0400

    Container With Most Water

---
+++ src/main/java/leetcode/solutions/math/ContainerWMtWater.java
@@ -0,0 +1,25 @@
+package leetcode.solutions.math;
+
+public class ContainerWMtWater {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int h = Math.min(height[left], height[right]);
+            int area = width * h;
+
+            maxArea = Math.max(maxArea, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
