006ca7a7a7671acee8d2072765c32539eeaaa939
tamarkilasonia <t.kilasonia2006@gmail.com>
2026-02-25 00:14:23 +0400

    PaliandromeNumberD1

--- src/main/java/leetcode/solutions/math/Main.java
+++ src/main/java/leetcode/solutions/math/Main.java
@@ -3,9 +3,18 @@ package leetcode.solutions.math;
 public class Main {
     public static void main(String[] args) {
         //ReverseInteger Day1
-          ReverseInteger rev = new ReverseInteger();
-            int result = rev.reverse(123);
-            System.out.println(result);
-        }
+        ReverseInteger rev = new ReverseInteger();
+        int result = rev.reverse(123);
+        System.out.println(result);
+
+        PalindromeNumber solution = new PalindromeNumber();
+
+//
+        int test = 121;
+
+        boolean isPaliandrome = solution.isPalindrome(test);
+
+        System.out.println("Is palindrome: " + isPaliandrome);
     }
 
+}
