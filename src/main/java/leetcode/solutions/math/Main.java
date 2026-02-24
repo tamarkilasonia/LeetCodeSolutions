84e723e117a0149caa07bac8eb2bd2dc4cfa077f
tamarkilasonia <t.kilasonia2006@gmail.com>
2026-02-25 00:07:30 +0400

    ReverseIntegerD1

---
+++ src/main/java/leetcode/solutions/math/Main.java
@@ -0,0 +1,11 @@
+package leetcode.solutions.math;
+
+public class Main {
+    public static void main(String[] args) {
+        //ReverseInteger Day1
+          ReverseInteger rev = new ReverseInteger();
+            int result = rev.reverse(123);
+            System.out.println(result);
+        }
+    }
+
