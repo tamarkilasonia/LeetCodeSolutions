f4f077a63d872fd3ba2042dad1a0bea0835c2156
Tamarkilasonia <tamarkilasonia@192.168.1.13>
2026-03-06 21:23:20 +0400

    Base 7

---
+++ src/main/java/leetcode/solutions/math/Base7.java
@@ -0,0 +1,24 @@
+package leetcode.solutions.math;
+
+public class Base7 {
+    public String convertToBase7(int num) {
+        if (num == 0) return "0";
+
+        boolean negative = num < 0;
+        num = Math.abs(num);
+
+        StringBuilder result = new StringBuilder();
+
+        while (num > 0) {
+            result.append(num % 7);
+            num /= 7;
+        }
+
+        if (negative) {
+            result.append("-");
+        }
+
+        return result.reverse().toString();
+    }
+}
+
