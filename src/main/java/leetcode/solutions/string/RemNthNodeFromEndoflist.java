9801db952b0e325b8fdf77c4c7a63d3cb13acfe9
tamarkilasonia <t.kilasonia2006@gmail.com>
2026-03-22 23:01:51 +0400

    Remove nth node from end of the list

---
+++ src/main/java/leetcode/solutions/string/RemNthNodeFromEndoflist.java
@@ -0,0 +1,29 @@
+package leetcode.solutions.string;
+
+public class RemNthNodeFromEndoflist {
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+
+
+            ListNode fast = dummy;
+            ListNode slow = dummy;
+
+
+            for (int i = 0; i <= n; i++) {
+                fast = fast.next;
+            }
+
+
+            while (fast != null) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+
+
+            slow.next = slow.next.next;
+
+            return dummy.next;
+        }
+}
