af1abae1879bf471dde24bb259fe3d1fe72532a2
tamarkilasonia <t.kilasonia2006@gmail.com>
2026-03-30 22:56:53 +0400

    interleaving String

---
+++ src/main/java/leetcode/solutions/string/validAnagram.java
@@ -0,0 +1,22 @@
+package leetcode.solutions.string;
+import java.util.HashMap;
+import java.util.Map;
+
+public class validAnagram {
+        public boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) return false;
+
+            int[] count = new int[26];
+
+            for (int i = 0; i < s.length(); i++) {
+                count[s.charAt(i) - 'a']++;
+                count[t.charAt(i) - 'a']--;
+            }
+
+            for (int c : count) {
+                if (c != 0) return false;
+            }
+
+            return true;
+        }
+    }
