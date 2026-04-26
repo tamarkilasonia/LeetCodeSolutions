81deb8a765234bfbc113c49969872515d10c584f
tamarkilasonia <t.kilasonia2006@gmail.com>
2026-04-26 12:00:00 +0400

    Paliandrome Partitioning

---
+++ src/main/java/leetcode/solutions/string/PalindromePartitioning.java
@@ -0,0 +1,36 @@
+package leetcode.solutions.string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PalindromePartitioning {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(s, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(String s, int start, List<String> path, List<List<String>> result) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int end = start; end < s.length(); end++) {
+            if (isPalindrome(s, start, end)) {
+                path.add(s.substring(start, end + 1));
+                backtrack(s, end + 1, path, result);
+                path.remove(path.size() - 1); // backtrack
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
